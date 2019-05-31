f6e1f487c0c124466a953eb6fe81ef328589c41c
燕琅 <tom.wzl@alibaba-inc.com>
2019-05-31 22:27:12 +0800

    add
    Signed-off-by: 燕琅 <tom.wzl@alibaba-inc.com>

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * 作者: yanlang.wzl 时间: 2019/5/31 工程: jjvm
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        //System.out.println(HelloWorld.class.getResource("/").toString());
+        System.out.println("hello world");
+    }
+}
--- src/main/java/org/caoym/jjvm/JJvm.java
+++ src/main/java/org/caoym/jjvm/JJvm.java
@@ -7,15 +7,19 @@ import java.util.Arrays;
  * Created by caoyangmin on 2017/9/7.
  */
 public class JJvm {
+    /**
+     *args ${rootPath}/jjvm/target/classes/ HelloWorld
+     * @param args
+     */
     static public void main(String[] args){
-        if(args.length == 0){
+        if(args.length <2 ){
             System.out.println("Usage: <classpath> <JJvm class> [args...]");
             return;
         }
         VirtualMachine vm = new VirtualMachine(Paths.get(args[0]), args[1]);
         try {
             args = Arrays.copyOfRange(args, 2, args.length);
-            vm.run(args);
+            vm.run(new String[1]);
         } catch (Exception e) {
             e.printStackTrace();
         }
